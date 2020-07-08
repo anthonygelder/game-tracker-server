027a83d94b05d36972ce9222643a44a62ba1ede6
Anthony Gelder <anthony.gelder@gmail.com>
2020-07-07 22:31:37 -0700

    'tweak'

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const knex = require('knex')
 const app = require('./app')
-const { DB_URL } = require('./config')
+const { DATABASE_URL } = require('./config')
 const PORT = process.env.PORT || 8000
 
 const db = knex({
   client: 'pg',
-  connection: DB_URL,
+  connection: DATABASE_URL,
 })
 
 app.set('db', db)
