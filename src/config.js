2d0c2ddf26e4e8064ae44aa25813349d093ff41b
Anthony Gelder <anthony.gelder@gmail.com>
2020-07-12 09:38:04 -0700

    'hello'

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/gametracker',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/gametracker',
     JWT_SECRET: process.env.JWT_SECRET ,
     JWT_EXPIRY: process.env.JWT_EXPIRY || '20s'
 }
